b0d9b3d0f30f2a67d2d3b9985380ccdd1ffbfeae
ocoulson <o_coulson@hotmail.co.uk>
2016-02-21 17:06:56 +0000

    Reverted paddles to have autoplay set to off by default.

---
+++ Block Breaker/Assets/Scripts/Life.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Life : MonoBehaviour {
+	//Singleton
+	static Life instance = null;
+	public static int lives;
+	void awake() {
+		if (instance != null) {
+			Destroy (gameObject);
+		} else {
+			GameObject.DontDestroyOnLoad(gameObject);
+			instance = this;
+			lives = 3;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
