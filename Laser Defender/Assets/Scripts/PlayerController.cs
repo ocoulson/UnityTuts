27938944e52c1956c827bab1a87ee5b20c49fe68
ocoulson <o_coulson@hotmail.co.uk>
2016-02-22 23:08:12 +0000

    Created enemy spawner, ship with controls in laser defender.

--- Laser Defender/Assets/Scripts/PlayerController.cs
+++ Laser Defender/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,20 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float speed = 15.0f;
+	public float padding = 1f;
+
+	float maxX;
+	float minX;
+
+	void Start ()
+	{
+		float zDistance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftMost = Camera.main.ViewportToWorldPoint(new Vector3(0,0,zDistance));
+		Vector3 rightMost = Camera.main.ViewportToWorldPoint(new Vector3(1,0,zDistance));
+
+		minX = leftMost.x + padding;
+		maxX = rightMost.x - padding;
+	}
 
 	void Update ()
 	{
@@ -11,22 +25,26 @@ public class PlayerController : MonoBehaviour {
 
 		bool leftKey = Input.GetKey (KeyCode.LeftArrow);
 		bool rightKey = Input.GetKey (KeyCode.RightArrow);
-		bool upKey = Input.GetKey (KeyCode.UpArrow);
-		bool downKey = Input.GetKey (KeyCode.DownArrow);
+
 
 		if (leftKey && !rightKey) {
 			
-			this.transform.position += new Vector3(-speed * Time.deltaTime, 0,0);
+			this.transform.position += Vector3.left * speed * Time.deltaTime;
 		} else if (rightKey && !leftKey) {
 
-			this.transform.position += new Vector3(speed * Time.deltaTime, 0,0);
+			this.transform.position += Vector3.right * speed * Time.deltaTime;
 		}
 
-		if (upKey && !downKey) {
-			this.transform.position += new Vector3(0, speed * Time.deltaTime, 0);
-		} else if (downKey && !upKey) {
-			this.transform.position += new Vector3(0, -speed * Time.deltaTime, 0);
-		}
+		float newX = Mathf.Clamp(transform.position.x,minX,maxX);
+
+		transform.position = new Vector3(newX, transform.position.y, transform.position.z); 
+//		bool upKey = Input.GetKey (KeyCode.UpArrow);
+//		bool downKey = Input.GetKey (KeyCode.DownArrow);
+//		if (upKey && !downKey) {
+//			this.transform.position += Vector3.up * speed * Time.deltaTime;
+//		} else if (downKey && !upKey) {
+//			this.transform.position += Vector3.down * speed * Time.deltaTime;
+//		}
 
 	}
 }
